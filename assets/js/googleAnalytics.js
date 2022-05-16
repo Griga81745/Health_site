3b17d5b1b6f0b7e4ecda770717451767d860d87a
Griga81745 <75415245+Griga81745@users.noreply.github.com>
2022-05-16 03:27:43 +0300

    Update googleAnalytics.js

--- assets/js/googleAnalytics.js
+++ assets/js/googleAnalytics.js
@@ -1,6 +1,12 @@
 const measurement_id = `G-DSQP2F3R6V`;
 const api_secret = `<secret_value>`;
 
+POST /mp/collect HTTP/1.1
+HOST: www.google-analytics.com
+Content-Type: application/json
+<payload_data>
+
+
 fetch(`https://www.google-analytics.com/mp/collect?measurement_id=${measurement_id}&api_secret=${api_secret}`, {
   method: "POST",
   body: JSON.stringify({
